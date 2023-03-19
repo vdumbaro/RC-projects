10fa0eab0ca6a3efaead83a1d8ef406c60ddfc75
VesoCool <vdumbarov@gmail.com>
2023-03-19 09:56:28 +0100

    Car with PWM for main motor

--- Car/Car.ino
+++ Car/Car.ino
@@ -2,8 +2,17 @@
 /*
  * Phase 1:
  * Setup main car arduino board 
- * connect a relay on the D8 to control the motors. No revers.
- * Connect a servo motor D9 to control the stearing.
+ * the motor drive is with a rellay board
+ * the stearing drive is with a rellay board 
+ * communication is via the RC
+ */
+
+ /*
+ * Phase 2:
+ * Setup main car arduino board 
+ * the motor drive is with a rellay board
+ * the stearing drive is with a rellay board
+ * communication is via the RC
  */
 
 #define OFF 0
@@ -11,14 +20,15 @@
 #define RC_433_CONTROL 2
 #define RC_NRF_CONTROL 3
 #define RELAY 1
-#define SERVO 2
+#define PWM 2
+
 
 
 // Control the way car will receive commands
 #define RC_CONTROL RC_NRF_CONTROL
 
 // Control the way car main drive
-#define MAIN_MOTOR_POWER RELAY
+#define MAIN_MOTOR_POWER PWM
 
 // Control the way car stear drive
 #define STEAR_MOTOR_POWER RELAY
@@ -48,27 +58,31 @@
   #define SCK 13 //pin D13
   
   RH_NRF24 nrf24(CE, CSN);
-  byte receive_buf[3];
+  byte receive_buf[5];
   uint8_t len = sizeof(receive_buf);
   #define MY_ID 0x73
 #endif
 
-#if STEAR_MOTOR_POWER == SERVO
+#if STEAR_MOTOR_POWER == PWM
   #include <Servo.h>
   Servo myservo;  // create servo object to control a servo
   #endif
-  #if STEAR_MOTOR_POWER == RELAY
-  #define STEAR_MOTOR_PIN 2
-  #define STEAR_DIRECTION_PIN 3
-  #endif
+#if STEAR_MOTOR_POWER == RELAY
+  #define STEAR_MOTOR_PIN 4
+  #define STEAR_DIRECTION_PIN 5
+#endif
   
-  #if MAIN_MOTOR_POWER == RELAY
-  #define MAIN_MOTOR_PIN 4
-  #define MAIN_DIRECTION_PIN 5
+#if MAIN_MOTOR_POWER == RELAY
+  #define MAIN_MOTOR_PIN 3
+  #define MAIN_DIRECTION_PIN 2
+#endif
+#if MAIN_MOTOR_POWER == PWM
+  #define MAIN_MOTOR_PIN 3
+  #define MAIN_DIRECTION_PIN 2
 #endif
 
 
-unsigned char motor[2] = {0,0};
+unsigned char motor[] = {0,0};
 #define MAIN_MOTOR 0
 #define STEAR_MOTOR 1
 
@@ -89,8 +103,18 @@ int set_drive_state(){
       digitalWrite(MAIN_MOTOR_PIN,LOW);
       digitalWrite(MAIN_DIRECTION_PIN,HIGH);
     }
-    
-  #else 
+  #endif
+  #if MAIN_MOTOR_POWER == PWM
+    if (motor[MAIN_MOTOR] <= 100 ) {
+      //Serial.println("Forward");
+      analogWrite(MAIN_MOTOR_PIN,map(motor[MAIN_MOTOR],0,100,0,255));
+      digitalWrite(MAIN_DIRECTION_PIN,HIGH);
+    }
+    if (motor[MAIN_MOTOR] > 100) {
+      //Serial.println("Back");
+      analogWrite(MAIN_MOTOR_PIN,map(motor[MAIN_MOTOR]-100,0,100,0,255));
+      digitalWrite(MAIN_DIRECTION_PIN,LOW);
+    }
   #endif
   #if STEAR_MOTOR_POWER == RELAY
     if (motor[STEAR_MOTOR] <= 100){
@@ -109,7 +133,7 @@ int set_drive_state(){
       digitalWrite(STEAR_DIRECTION_PIN,HIGH);
     }
   #endif
-  #if STEAR_MOTOR_POWER == SERVO
+  #if STEAR_MOTOR_POWER == PWM
     myservo.write(motor[STEAR_MOTOR]); 
   #endif
 }
@@ -122,7 +146,7 @@ void setup() {
     digitalWrite(STEAR_MOTOR_PIN,HIGH);
     digitalWrite(STEAR_DIRECTION_PIN,HIGH);
   #endif
-  #if STEAR_MOTOR_POWER == SERVO
+  #if STEAR_MOTOR_POWER == PWM
     //myservo.attach(STEAR_PIN);  // attaches the servo on pin 9 to the servo object
    // myservo.write(motor[STEAR_MOTOR]);  
     
@@ -133,8 +157,11 @@ void setup() {
     digitalWrite(MAIN_MOTOR_PIN,HIGH);
     digitalWrite(MAIN_DIRECTION_PIN,HIGH);
   #endif
-  #if MAIN_MOTOR_POWER == SERVO
-    // add PWM for MAIN_MOTOR 
+  #if MAIN_MOTOR_POWER == PWM
+    pinMode(MAIN_MOTOR_PIN, OUTPUT);
+    pinMode(MAIN_DIRECTION_PIN, OUTPUT);
+    digitalWrite(MAIN_MOTOR_PIN,HIGH);
+    digitalWrite(MAIN_DIRECTION_PIN,HIGH); 
   #endif
 
   #if RC_CONTROL == RC_433_CONTROL
@@ -219,7 +246,6 @@ void loop() {
     if (nrf24.available())
     {
       // Should be a message for us now   
-      len = 3;
       if (nrf24.recv(receive_buf, &len))
       {
         //NRF24::printBuffer("request: ", buf, len);
@@ -236,21 +262,9 @@ void loop() {
         Serial.println("recv failed");
       }
       if (receive_buf[0] == MY_ID) {
-        if (receive_buf[1] < 120 || receive_buf[1] > 140){ 
-          motor[MAIN_MOTOR]= receive_buf[1];}
-        else {
-          motor[MAIN_MOTOR]= 128;
-        }
-        Serial.print(motor[MAIN_MOTOR]);
-
-        if (receive_buf[2] < 120 || receive_buf[2] > 140){ 
-          motor[STEAR_MOTOR]= receive_buf[2];}
-        else {
-          motor[STEAR_MOTOR]= 128;
-        }
-        Serial.print(motor[STEAR_MOTOR]);
+        motor[MAIN_MOTOR]= receive_buf[1];
+        motor[STEAR_MOTOR]= receive_buf[2];
       }
-      Serial.println("");
     }
   #endif
   
